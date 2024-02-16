204c16eb66672f2db9e54a7b9d76ce01064dcaba
Cristina Ramirez <crami23@emory.edu>
2024-02-16 16:48:08 -0500

    check

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,44 @@
+<template>
+  <div>
+    <h1 class="mb-4">Foodiology</h1>
+    <div class="container">
+      <div class="btn-group">
+        <button class="btn btn-primary dropdown-toggle" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Select Ingredients
+        </button>
+        <div class="dropdown-menu">
+          <a class="dropdown-item" href="#">Ingredient 1</a>
+          <a class="dropdown-item" href="#">Ingredient 2</a>
+          <a class="dropdown-item" href="#">Ingredient 3</a>
+          <!-- Add more items as needed -->
+        </div>
+      </div>
+
+      <div class="form-check mt-3" v-if="checked">
+        <input class="form-check-input" type="checkbox" value="" id="checkbox1">
+        <label class="form-check-label" for="checkbox1">
+          Recipe Option 1
+        </label>
+      </div>
+      <div class="form-check" v-if="checked">
+        <input class="form-check-input" type="checkbox" value="" id="checkbox2">
+        <label class="form-check-label" for="checkbox2">
+          Recipe Option 2
+        </label>
+      </div>
+
+      <button class="btn btn-primary mt-3" @click="showInput">Search for Recipes</button>
+      <input v-if="checked" type="text" class="form-control mt-3" placeholder="Search for recipes...">
+    </div>
+  </div>
+</template>
+
 <script setup>
 import { ref } from 'vue'
+
 const checked = ref(false)
+
 const showInput = () => {
   checked.value = !checked.value
 }
 </script>
-<template>
-  <div>
-    <h1>Foodiology</h1>
-    <div class="container">
-      <button class="btn btn-primary" @click="showInput">Input Ingredients</button>
-      <input v-if="checked" type="text" class="form-control mt-3" placeholder="Search for recipes...">
-    </div>
-  </div>
-
-</template>
