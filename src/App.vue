4a7511f1525c78d85221c23c77d00f1b666152e2
Cristina Ramirez <crami23@emory.edu>
2024-02-16 16:29:47 -0500

    First Commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { ref } from 'vue'
+const checked = ref(false)
+const showInput = () => {
+  checked.value = !checked.value
+}
+</script>
+
+
+<template>
+  <div>
+    <h1>Foodiology</h1>
+    <div class="container">
+      <button class="btn btn-primary" @click="showInput">Input Ingredients</button>
+      <input v-if="checked" type="text" class="form-control mt-3" placeholder="Search for recipes...">
+    </div>
+  </div>
+
+</template>
---
+++ src/main.js
@@ -0,0 +1,9 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+
+import './assets/main.css'
+import 'bootstrap/dist/css/bootstrap.css'
+import bootstrap from 'bootstrap/dist/js/bootstrap.bundle.js'
+
+
+createApp(App).use(bootstrap).mount('#app')
