a878f7dbb62111b164c14f1ed99b93941661e949
Cristina Ramirez <cristina.ramirez@emory.edu>
2024-03-04 12:19:00 -0500

    first commit

---
+++ Foodiology_FrontEnd/src/views/SearchView.vue
@@ -0,0 +1,88 @@
+<template>
+    <div class="max-w-7xl mx-auto grid grid-cols-4 gap-4">
+        <div class="main-left col-span-3 space-y-4">
+            <div class="bg-white border border-gray-200 rounded-lg">
+                <div class="p-4 flex space-x-4">  
+                    <input type="search" class="p-4 w-full bg-gray-100 rounded-lg" placeholder="Enter your ingredients with a space:">
+
+                    <a href="#" class="inline-block py-4 px-6 bg-purple-600 text-white rounded-lg">Post</a>
+                </div>
+            </div>
+
+            <div class="p-4 bg-white border border-gray-200 rounded-lg grid grid-cols-4 gap-4">
+                <div class="p-4 text-center bg-gray-100 rounded-lg">
+                    <img src="@/assets/logo copy.png" class="mb-6 rounded-full">
+                
+                    <p><strong>recipe</strong></p>
+
+                    <div class="mt-6 flex space-x-8 justify-around">
+                        <p class="text-xs text-gray-500">ingredients you have</p>
+                        <p class="text-xs text-gray-500">ingredients you need</p>
+                    </div>
+                </div>
+
+                <div class="p-4 text-center bg-gray-100 rounded-lg">
+                    <img src="@/assets/logo copy.png" class="mb-6 rounded-full">
+                
+                    <p><strong>recipe</strong></p>
+
+                    <div class="mt-6 flex space-x-8 justify-around">
+                        <p class="text-xs text-gray-500">ingredients you have</p>
+                        <p class="text-xs text-gray-500">ingredients you need</p>
+                    </div>
+                </div>
+
+                <div class="p-4 text-center bg-gray-100 rounded-lg">
+                    <img src="@/assets/logo copy.png" class="mb-6 rounded-full">
+                
+                    <p><strong>recipe</strong></p>
+
+                    <div class="mt-6 flex space-x-8 justify-around">
+                        <p class="text-xs text-gray-500">ingredients you have</p>
+                        <p class="text-xs text-gray-500">ingredients you need</p>
+                    </div>
+                </div>
+
+                <div class="p-4 text-center bg-gray-100 rounded-lg">
+                    <img src="@/assets/logo copy.png" class="mb-6 rounded-full">
+                
+                    <p><strong>recipe</strong></p>
+
+                    <div class="mt-6 flex space-x-8 justify-around">
+                        <p class="text-xs text-gray-500">ingredients you have</p>
+                        <p class="text-xs text-gray-500">ingredients you need</p>
+                    </div>
+                </div>
+                
+                <div class="p-4 text-center bg-gray-100 rounded-lg">
+                    <img src="@/assets/logo copy.png" class="mb-6 rounded-full">
+                
+                    <p><strong>recipe</strong></p>
+
+                    <div class="mt-6 flex space-x-8 justify-around">
+                        <p class="text-xs text-gray-500">ingredients you have</p>
+                        <p class="text-xs text-gray-500">ingredients you need</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="main-right col-span-1 space-y-4">
+            <RecommendedRecipes/>
+            <TrendingRecipes/>              
+        </div>
+    </div>
+</template>
+
+<script>
+import RecommendedRecipes from '../components/RecommendedRecipes.vue'
+import TrendingRecipes from '../components/TrendingRecipes.vue';
+
+export default {
+    name: 'SearchView',
+    components: {
+        RecommendedRecipes,
+        TrendingRecipes,
+    }
+}
+</script>
