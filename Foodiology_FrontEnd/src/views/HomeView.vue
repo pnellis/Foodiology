f0c8674937fd76005c122f5a6198206d8b357203
Cristina Ramirez <cristina.ramirez@emory.edu>
2024-03-05 13:45:09 -0500

    home page

--- Foodiology_FrontEnd/src/views/HomeView.vue
+++ Foodiology_FrontEnd/src/views/HomeView.vue
@@ -1,9 +1,10 @@
 <template>
-  <main>
-    <div class="welcome-container">
+  <main class="main-background">
+    <div class="welcome-container welcome-background">
       <!-- <img src="@/assets/HomePhoto.jpg" alt="Delicious food" class="home-photo"> -->
       <h1>Welcome to Foodiology!</h1>
-      <p>Discover, Share, and Enjoy Amazing Recipes.</p>
+      <p>Transforming kitchen confusion into delicious discoveries.</p>
+          <p>Discover delicious recipes based on your ingredients and preferences!</p>
       <RouterLink to="/search" class="search-button">
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" class="w-6 h-6">
           <path stroke-linecap="round" stroke-linejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z"></path>
@@ -14,26 +15,37 @@
   </main>
 </template>
 <style scoped>
+
+
 main {
   display: flex;
   justify-content: center;
-  align-items: center;
-  height: 100vh;
+  
+  height: 50vh;
 }
 .welcome-container {
   text-align: center;
-  max-width: 600px;
+  max-width: 800px;
+  max-height: 300px;
 }
+
+.welcome-background {
+  background-color: #f792ac; /* Set the background color to #f792ac */
+  padding: 10px; /* Optional: Add padding for better visual appearance */
+}
+
 h1 {
   font-size: 3rem;
   margin-bottom: 16px;
-  color: #4c51bf;
+  color: #464040;
+  font-weight: bold;
 }
 
 p {
   font-size: 1.5rem;
   margin-bottom: 24px;
-  color: #6b7280;
+  color: #464040;
+  font-weight: bold;
 }
 
 .search-button {
@@ -42,7 +54,7 @@ p {
   justify-content: center;
   padding: 12px 24px;
   font-size: 1.25rem;
-  background-color: #6b7280;
+  background-color: rgb(134, 212, 208);
   color: white;
   text-decoration: none;
   border-radius: 8px;
@@ -50,13 +62,13 @@ p {
 }
 
 .search-button:hover {
-  background-color: #4c51bf;
+  background-color: rgb(52, 136, 131);
 }
 
-.home-background {
+main.main-background {
   background-image: url("@/assets/HomePhoto.jpg");
   background-size: contain;
-  background-position: center;
+  
   background-repeat: no-repeat;
   height: 200vh; /* Adjust the height as needed */
   margin: 0;
