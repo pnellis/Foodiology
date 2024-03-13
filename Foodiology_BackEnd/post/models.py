1ce1740692a73f54de73c43837bb7454427923a4
audreyp00n <apoon5@emory.edu>
2024-03-13 18:40:44 -0400

    created model

---
+++ Foodiology_BackEnd/post/models.py
@@ -0,0 +1,39 @@
+import uuid
+from django.db import models
+
+# Create your models here.
+from django.utils.timesince import timesince
+
+# not implemented yet ?
+# from account.models import User
+
+
+class Like(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    created_by = models.ForeignKey(User, related_name='likes', on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+class PostAttachment(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    image = models.ImageField(upload_to='post_attachments')
+    created_by = models.ForeignKey(User, related_name='post_attachments', on_delete=models.CASCADE)
+
+
+class Post(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    body = models.TextField(blank=True, null=True)
+
+    attachments = models.ManyToManyField(PostAttachment, blank=True)
+
+    likes = models.ManyToManyField(Like, blank=True)
+    likes_count = models.IntegerField(default=0)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    created_by = models.ForeignKey(User, related_name='posts', on_delete=models.CASCADE)
+
+    class Meta:
+        ordering = ('-created_at',)
+    
+    def created_at_formatted(self):
+       return timesince(self.created_at)
