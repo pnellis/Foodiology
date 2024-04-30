4cbd0c5e8ae5092c90ce2b39361f966ca7f501f1
zz-carol <carol.zhou@emory.edu>
2024-04-30 20:44:45 +0000

    Fixed gitignore and env file

--- Foodiology_BackEnd/Foodiology_BackEnd/settings.py
+++ Foodiology_BackEnd/Foodiology_BackEnd/settings.py
@@ -1,5 +1,6 @@
 from datetime import timedelta
 from pathlib import Path
+import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
